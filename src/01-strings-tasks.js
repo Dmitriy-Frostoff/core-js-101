74830ddc4466ef92ceab16936ede396b5d78efa2
dmitriy-frostoff <dmitriy.frostoff@yandex.ru>
2023-08-28 04:00:42 +0300

    feat: implement extractNameFromTemplate function

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -72,8 +72,10 @@ function getStringFromTemplate(firstName, lastName) {
  *   'Hello, John Doe!' => 'John Doe'
  *   'Hello, Chuck Norris!' => 'Chuck Norris'
  */
-function extractNameFromTemplate(/* value */) {
-  throw new Error('Not implemented');
+function extractNameFromTemplate(value) {
+  return value
+    .replace(/Hello,\s+/, '')
+    .replace(/!$/, '');
 }
 
 
