0576e0f611a1b37aaf6ad2a52ec00239a74d7e7c
dmitriy-frostoff <dmitriy.frostoff@yandex.ru>
2023-08-31 04:59:35 +0300

    feat: implement reverseString function

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -264,8 +264,11 @@ function getIntervalString(a, b, isStartIncluded, isEndIncluded) {
  * 'rotator' => 'rotator'
  * 'noon' => 'noon'
  */
-function reverseString(/* str */) {
-  throw new Error('Not implemented');
+function reverseString(str) {
+  return str
+    .split('')
+    .reverse()
+    .join('');
 }
 
 
