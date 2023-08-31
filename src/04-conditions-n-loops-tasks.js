40d5bf00f7a32e53fabaddfcbed5029425bdd307
dmitriy-frostoff <dmitriy.frostoff@yandex.ru>
2023-08-31 23:20:12 +0300

    feat: implement toNaryString function

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -463,8 +463,12 @@ function isBracketsBalanced(str) {
  *    365, 4  => '11231'
  *    365, 10 => '365'
  */
-function toNaryString(/* num, n */) {
-  throw new Error('Not implemented');
+function toNaryString(num, n) {
+  if (!Number.isFinite(num) || !Number.isFinite(n)) {
+    throw new Error('num and n must be integer numbers!');
+  }
+
+  return num.toString(n);
 }
 
 
