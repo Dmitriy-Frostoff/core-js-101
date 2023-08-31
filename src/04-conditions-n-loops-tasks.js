ddbde12d2d6260ce8b152a48f81818d2bb043797
dmitriy-frostoff <dmitriy.frostoff@yandex.ru>
2023-08-31 05:18:18 +0300

    feat: implement getDigitalRoot function

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -330,8 +330,18 @@ function isCreditCardNumber(/* ccn */) {
  *   10000 ( 1+0+0+0+0 = 1 ) => 1
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
-function getDigitalRoot(/* num */) {
-  throw new Error('Not implemented');
+function getDigitalRoot(num) {
+  let result = num;
+
+  if (Math.abs(result) < 10) {
+    return result;
+  }
+
+  result = String(result)
+    .split('')
+    .reduce((sum, stringNum) => sum + +stringNum, 0);
+
+  return getDigitalRoot(result);
 }
 
 
